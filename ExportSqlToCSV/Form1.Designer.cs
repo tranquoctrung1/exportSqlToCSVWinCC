35a4426c608511a1849c130b8dab382a21d2c7e0
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2022-10-20 14:47:02 +0700

    init source

---
+++ ExportSqlToCSV/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ExportSqlToCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grv = new System.Windows.Forms.DataGridView();
+            this.btnExportCSV = new System.Windows.Forms.Button();
+            this.txtTableName = new System.Windows.Forms.Label();
+            this.btnNextTable = new System.Windows.Forms.Button();
+            this.txtTableIndex = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grv
+            // 
+            this.grv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grv.Location = new System.Drawing.Point(12, 75);
+            this.grv.Name = "grv";
+            this.grv.Size = new System.Drawing.Size(776, 346);
+            this.grv.TabIndex = 0;
+            // 
+            // btnExportCSV
+            // 
+            this.btnExportCSV.Location = new System.Drawing.Point(449, 28);
+            this.btnExportCSV.Name = "btnExportCSV";
+            this.btnExportCSV.Size = new System.Drawing.Size(164, 23);
+            this.btnExportCSV.TabIndex = 1;
+            this.btnExportCSV.Text = "ExportCVS";
+            this.btnExportCSV.UseVisualStyleBackColor = true;
+            this.btnExportCSV.Click += new System.EventHandler(this.btnExportCSV_Click);
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.AutoSize = true;
+            this.txtTableName.Location = new System.Drawing.Point(13, 28);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(0, 13);
+            this.txtTableName.TabIndex = 2;
+            // 
+            // btnNextTable
+            // 
+            this.btnNextTable.Location = new System.Drawing.Point(201, 28);
+            this.btnNextTable.Name = "btnNextTable";
+            this.btnNextTable.Size = new System.Drawing.Size(75, 23);
+            this.btnNextTable.TabIndex = 3;
+            this.btnNextTable.Text = "Next Table";
+            this.btnNextTable.UseVisualStyleBackColor = true;
+            this.btnNextTable.Click += new System.EventHandler(this.btnNextTable_Click);
+            // 
+            // txtTableIndex
+            // 
+            this.txtTableIndex.AutoSize = true;
+            this.txtTableIndex.Location = new System.Drawing.Point(103, 28);
+            this.txtTableIndex.Name = "txtTableIndex";
+            this.txtTableIndex.Size = new System.Drawing.Size(0, 13);
+            this.txtTableIndex.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTableIndex);
+            this.Controls.Add(this.btnNextTable);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.btnExportCSV);
+            this.Controls.Add(this.grv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grv;
+        private System.Windows.Forms.Button btnExportCSV;
+        private System.Windows.Forms.Label txtTableName;
+        private System.Windows.Forms.Button btnNextTable;
+        private System.Windows.Forms.Label txtTableIndex;
+    }
+}
+
---
+++ ExportSqlToCSV/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportSqlToCSV
+{
+    public partial class Form1 : Form
+    {
+        string conString = $"Data Source=TQTR;Initial Catalog=CC_ES_22_08_22_13_29_33R;Integrated Security=SSPI;";
+        List<string> listTableName = new List<string>();
+        int countTable = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon;
+            sqlCon = new SqlConnection(conString);
+            sqlCon.Open();
+
+            GetAllTableName();
+
+            if(listTableName.Count > 0)
+            {
+                for(; countTable < listTableName.Count; countTable ++)
+                {
+                    string table = listTableName[countTable];
+                    txtTableIndex.Text = countTable.ToString();
+
+                    txtTableName.Text = table;
+
+                    string sqlQuery = $"select * from {table}";
+                    SqlDataAdapter dscmd = new SqlDataAdapter(sqlQuery, sqlCon);
+                    DataTable dtData = new DataTable();
+                    dscmd.Fill(dtData);
+                    grv.DataSource = dtData;
+
+                    string pathFile = @"C:\Users\TQTr\Desktop\CC_ES_22_08_22_13_29_33R\" + table + ".csv";
+                    bool fileError = false;
+                    if (File.Exists(pathFile))
+                    {
+                        try
+                        {
+                            File.Delete(pathFile);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            int columnCount = grv.Columns.Count;
+                            string columnNames = "";
+                            string[] outputCsv = new string[grv.Rows.Count + 1];
+                            for (int i = 0; i < columnCount; i++)
+                            {
+                                columnNames += grv.Columns[i].HeaderText.ToString() + ",";
+                            }
+                            outputCsv[0] += columnNames;
+
+                            for (int i = 1; (i - 1) < grv.Rows.Count; i++)
+                            {
+                                for (int j = 0; j < columnCount; j++)
+                                {
+                                    if (grv.Rows[i - 1].Cells[j] != null)
+                                    {
+                                        if (grv.Rows[i - 1].Cells[j].Value != null)
+                                        {
+                                            outputCsv[i] += grv.Rows[i - 1].Cells[j].Value.ToString() + ",";
+                                        }
+                                        else
+                                        {
+                                            outputCsv[i] += " ,";
+                                        }
+                                    }
+                                    else
+                                    {
+                                        outputCsv[i] += " ,";
+                                    }
+
+                                }
+                            }
+
+                            File.WriteAllLines(pathFile, outputCsv, Encoding.UTF8);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+
+                }
+            }
+            
+            sqlCon.Close();
+            MessageBox.Show("Exported File Done!!");
+        }
+
+        public void GetAllTableName()
+        {
+            string sqlQuery = $"SELECT TABLE_NAME\r\nFROM INFORMATION_SCHEMA.TABLES\r\nWHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='CC_ES_22_08_22_13_29_33R'";
+
+            SqlConnection sqlCon = new SqlConnection(conString);
+
+            sqlCon.Open();
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, sqlCon);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if(reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    string table = "";
+
+                    try
+                    {
+                        table = reader["TABLE_NAME"].ToString();
+                    }
+                    catch(Exception ex)
+                    {
+                        table = "";
+                    }
+
+                    listTableName.Add(table);
+                }
+            }
+        }
+
+        private void btnExportCSV_Click(object sender, EventArgs e)
+        {
+            if (grv.Rows.Count > 0)
+            {
+                string table = listTableName[countTable];
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "CSV (*.csv)|*.csv";
+                sfd.FileName = $"{table}.csv";
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            int columnCount = grv.Columns.Count;
+                            string columnNames = "";
+                            string[] outputCsv = new string[grv.Rows.Count + 1];
+                            for (int i = 0; i < columnCount; i++)
+                            {
+                                columnNames += grv.Columns[i].HeaderText.ToString() + ",";
+                            }
+                            outputCsv[0] += columnNames;
+
+                            for (int i = 1; (i - 1) < grv.Rows.Count; i++)
+                            {
+                                for (int j = 0; j < columnCount; j++)
+                                {
+                                    if(grv.Rows[i - 1].Cells[j] != null)
+                                    {
+                                        if (grv.Rows[i - 1].Cells[j].Value != null)
+                                        {
+                                            outputCsv[i] += grv.Rows[i - 1].Cells[j].Value.ToString() + ",";
+                                        }
+                                        else
+                                        {
+                                            outputCsv[i] += " ,";
+                                        }
+                                    }
+                                    else
+                                    {
+                                        outputCsv[i] += " ,";
+                                    }
+                                    
+                                }
+                            }
+
+                            File.WriteAllLines(sfd.FileName, outputCsv, Encoding.UTF8);
+                            MessageBox.Show("Data Exported Successfully !!!", "Info");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Record To Export !!!", "Info");
+            }
+
+        }
+
+        private void btnNextTable_Click(object sender, EventArgs e)
+        {
+            countTable++;
+
+            SqlConnection sqlCon;
+            sqlCon = new SqlConnection(conString);
+            sqlCon.Open();
+
+            if (listTableName.Count > 0)
+            {
+                string table = listTableName[countTable];
+                txtTableIndex.Text = countTable.ToString();
+
+                txtTableName.Text = table;
+
+                string sqlQuery = $"select * from {table}";
+                SqlDataAdapter dscmd = new SqlDataAdapter(sqlQuery, sqlCon);
+                DataTable dtData = new DataTable();
+                dscmd.Fill(dtData);
+                grv.DataSource = dtData;
+            }
+
+            sqlCon.Close();
+        }
+    }
+}
